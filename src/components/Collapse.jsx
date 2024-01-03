1e75256926756f1006e6fd32e0b098cda21ce4da
ananthumaheswaran <ananthumaheswaran098@gmail.com>
2024-01-03 14:36:16 +0530

    className changed for svg icons and ternary operator is added to decide the appropriate heading for the condition

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -9,20 +9,24 @@ const Collapse = ({ title, children }) => {
   return (
     <>
       <div className="collapse-section">
-        <div onClick={toggleSection} className="collapse-header">
-          {title}
-        </div>
+        {title ? (
+          <div className="collapse-header">{title}</div>
+        ) : (
+          <div className="collapse-header">
+            Click the edit icon to add details
+          </div>
+        )}
         <div onClick={toggleSection} className="collapse-header2">
           {isExpanded ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
-              className="w-5 h-5"
+              className="toggle-icon-size"
             >
               <path
                 fillRule="evenodd"
-                d="M14.77 12.79a.75.75 0 01-1.06-.02L10 8.832 6.29 12.77a.75.75 0 11-1.08-1.04l4.25-4.5a.75.75 0 011.08 0l4.25 4.5a.75.75 0 01-.02 1.06z"
+                d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM8.28 7.22a.75.75 0 0 0-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 1 0 1.06 1.06L10 11.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L11.06 10l1.72-1.72a.75.75 0 0 0-1.06-1.06L10 8.94 8.28 7.22Z"
                 clipRule="evenodd"
               />
             </svg>
@@ -31,13 +35,10 @@ const Collapse = ({ title, children }) => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
-              className="w-5 h-5"
+              className="toggle-icon-size"
             >
-              <path
-                fillRule="evenodd"
-                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                clipRule="evenodd"
-              />
+              <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
+              <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
             </svg>
           )}
         </div>
